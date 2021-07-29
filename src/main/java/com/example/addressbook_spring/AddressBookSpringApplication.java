b8ab80c7356482f03a300ae6c950e42a84718ba4
Sonali265 <Sonalibhardwajhjp@gmail.com>
2021-07-29 12:55:46 +0530

    Use Lombok Library For Logging

--- src/main/java/com/example/addressbook_spring/AddressBookSpringApplication.java
+++ src/main/java/com/example/addressbook_spring/AddressBookSpringApplication.java
@@ -1,13 +1,16 @@
 package com.example.addressbook_spring;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class AddressBookSpringApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AddressBookSpringApplication.class, args);
+        log.info("Address Book App Started");
     }
 
 }

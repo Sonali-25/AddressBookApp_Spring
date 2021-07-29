100aaabb03fb52864a383417460aeb1be0e10232
Sonali265 <Sonalibhardwajhjp@gmail.com>
2021-07-29 14:01:58 +0530

    Database Setting And Environment Variables

--- src/main/java/com/example/addressbook_spring/AddressBookSpringApplication.java
+++ src/main/java/com/example/addressbook_spring/AddressBookSpringApplication.java
@@ -3,14 +3,21 @@ package com.example.addressbook_spring;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @Slf4j
 public class AddressBookSpringApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AddressBookSpringApplication.class, args);
-        log.info("Address Book App Started");
+        ApplicationContext context=SpringApplication.run(AddressBookSpringApplication.class, args);
+        log.info("Address Book App Started in {} Environment",
+                context.getEnvironment().getProperty("environment"));
+
+        log.info("Address Book Database User is {}",
+                context.getEnvironment().getProperty("spring.datasource.username"));
     }
 
 }
+
+
